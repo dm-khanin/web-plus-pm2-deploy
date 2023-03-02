ba59de5c49db0cdab4fa71e152152d9c849b048a
Dmitry Khanin  😎 <dm.khanin@gmail.com>
2023-03-03 00:30:54 +0200

    deploy: configure frontend deploy script

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config({
+  path: '.env.deploy',
+});
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_REPO, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && npm i && npm run build',
+    },
+  },
+};
